38d3e767dd1caddcd696298e984ba6c3692b715f
Laurentz-94 <lorenzocasabianca94@gmail.com>
2019-05-14 18:22:09 +0200

    funzioni di creazione modifica e eliminazione utente

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,49 @@ const {check} = require('express-validator/check');
 
 const User = require('../models/users');
 
+router.post('/', [
+    check('name').exists(),
+    check('surname').exists(),
+    check('email').isEmail(),
+    check('age').isInt(),
+  ],
+  function(req, res) {
+    const newUser = new User(req.body);
+    newUser.save(function(err) {
+      if (err) return res.status(500).json({error: err});
+      res.status(201).json(newUser);
+    });
+  });
+  
+  router.put('/:id', function(req, res) {
+    User.findOne({_id: req.params.id})
+        .exec(function(err, user) {
+          if (err) return res.status(500).json({error: err});
+          if (!user) {
+            return res.status(404).json({message: 'Utente non trovato'});
+          }
+          for (key in req.body) {
+            if (req.body.hasOwnProperty(key)) {
+              user[key] = req.body[key];
+            }
+          }
+          user.save(function(err) {
+            if (err) return res.status(500).json({error: err});
+            res.json(user);
+          });
+        });
+  });
+  
+  router.delete('/:id', function(req, res) {
+    User.findOne({_id: req.params.id})
+        .exec(function(err, user) {
+          if (err) return res.status(500).json({error: err});
+          if (!user) return res.status(404).json({message: 'Utente non trovato'});
+          User.remove({_id: req.params.id}, function(err) {
+            if (err) return res.status(500).json({error: err});
+            res.json({message: 'Utente eliminato correttamente'});
+          });
+        });
+  });
 app.listen(3000);
 module.exports = router;
